59ec36e496ea0c3a2d2cfec414d258c2cf3932d8
jltimm <jacobltimm@gmail.com>
2018-02-08 17:45:14 -0600

    Adding new folders, setting up the directory structure.

---
+++ convolutions/sobel.go
@@ -0,0 +1,10 @@
+package convolutions
+
+import (
+	"fmt"
+)
+
+func Sobel() {
+	fmt.Printf("Hello, world!\n")
+	fmt.Printf("Hey again!\n")
+}
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/jltimm/go-image-processing/convolutions"
 )
 
 func main() {
 	fmt.Printf("Hello, world!\n")
 	fmt.Printf("Hey again!\n")
+	convolutions.Sobel()
 }
