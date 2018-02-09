a875275fd5397d2b8a7ce91259e0690501df673f
jltimm <jacobltimm@gmail.com>
2018-02-08 20:48:09 -0600

    Added ability to read file that's passed in via command line

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"os"
+
+	"github.com/jltimm/go-image-processing/convolutions"
 )
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	args := os.Args[1:]
 	if len(args) >= 2 {
 		conv := args[0]
 		fileName := args[1]
-		fmt.Println(conv)
-		fmt.Println(fileName)
-		//TODO: search for file
+
+		data, err := ioutil.ReadFile(fileName)
+		check(err)
+
+		if len(data) >= 0 && conv == "sobel" {
+			convolutions.Sobel()
+		}
 	}
 }
