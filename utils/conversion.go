30a7a56b3778e975fd24899cff3c421e2a4e056b
jltimm <jacobltimm@gmail.com>
2018-02-10 12:39:54 -0600

    Added method that retrieves the file extension

--- utils/conversion.go
+++ utils/conversion.go
@@ -1,10 +1,19 @@
 package utils
 
 import (
+	"fmt"
 	"image"
 	"image/png"
 	"os"
+	"strings"
 )
+
+// getFileExtension takes as input a filename, and gets the extension
+func getFileExtension(filename string) string {
+	splitString := strings.Split(filename, ".")
+	ext := strings.ToLower(splitString[len(splitString)-1])
+	return ext
+}
 
 // CheckIfFileExists takes as input a filename, and checks if a file exists
 func CheckIfFileExists(filename string) bool {
@@ -16,6 +25,8 @@ func CheckIfFileExists(filename string) bool {
 
 // DecodeImage takes as input a filename, and returns the decoded Image data, and its bounds
 func DecodeImage(filename string) (image.Image, image.Rectangle) {
+	ext := getFileExtension(filename)
+	fmt.Println(ext)
 	//TODO: consider adding check to see if this throws an error. it shouldn't, but who knows
 	reader, _ := os.Open(filename)
 	defer reader.Close()
