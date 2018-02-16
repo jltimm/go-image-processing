85ac9ceb43a161171253413364dc19369a84bbe0
jltimm <jacobltimm@gmail.com>
2018-02-15 19:59:09 -0600

    Added TODOs

--- utils/encode.go
+++ utils/encode.go
@@ -6,7 +6,10 @@ import (
 	"os"
 )
 
+//TODO: add methods to create files from simply a filename input
+
 // CreateFileFromRGBA takes as input image data and encodes it
+// TODO: clean this up
 func CreateFileFromRGBA(filename string, convolutionName string, img *image.RGBA) {
 	outputFile, err := os.Create("test.png")
 	if err != nil {
