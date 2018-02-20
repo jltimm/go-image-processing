66da08ecc28daaeea0d3c160809350871be0618c
jltimm <jacobltimm@gmail.com>
2018-02-20 17:08:10 -0600

    Sobel work: added kernels and an empty getGradients function

--- convolutions/sobel.go
+++ convolutions/sobel.go
@@ -7,12 +7,32 @@ import (
 	"github.com/jltimm/go-image-processing/utils"
 )
 
+var (
+	kernelX = [][]int8{
+		{1, 0, -1},
+		{2, 0, -2},
+		{1, 0, -1},
+	}
+	kernelY = [][]int8{
+		{1, 2, 1},
+		{0, 0, 0},
+		{-1, -2, -1},
+	}
+)
+
+func getGradients(img image.Gray, x int, y int) (int, int) {
+
+	return x, y
+}
+
 func sobelOperator(img image.Gray) image.Gray {
 	bounds := img.Bounds()
-	for x := 0; x < bounds.Max.X; x++ {
-		for y := 0; y < bounds.Max.Y; y++ {
-			r, _, _, _ := img.At(x, y).RGBA()
-			fmt.Println(uint8(r))
+	for x := 1; x < bounds.Max.X-1; x++ {
+		for y := 1; y < bounds.Max.Y-1; y++ {
+			//r, _, _, _ := img.At(x, y).RGBA()
+			gx, gy := getGradients(img, x, y)
+			fmt.Println(gx)
+			fmt.Println(gy)
 		}
 	}
 	return img
@@ -27,8 +47,9 @@ func Sobel(filename string) image.Gray {
 
 	img := utils.ConvertToGrayscaleFromFilename(filename)
 	if img == nil {
-		// TODO: handle, there may be a better way to do this
+		panic("img returned nil")
 	}
+
 	sobelImg := sobelOperator(*img)
 	return sobelImg
 }
